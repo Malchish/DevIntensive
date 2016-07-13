f3e09069e882c35f2d4175fb071b004e3a5b9398
Malchish <malchih@tpu.ru>
2016-07-13 07:28:31 +0300

    Authorization are done

--- app/src/main/java/com/softdesign/devintensive/data/managers/DataManager.java
+++ app/src/main/java/com/softdesign/devintensive/data/managers/DataManager.java
@@ -1,11 +1,38 @@
 package com.softdesign.devintensive.data.managers;
 
+import android.content.ContentValues;
+import android.content.Context;
+
+import com.softdesign.devintensive.data.network.RestService;
+import com.softdesign.devintensive.data.network.ServiceGenerator;
+import com.softdesign.devintensive.data.network.req.UserLoginReq;
+import com.softdesign.devintensive.data.network.res.UserModelRes;
+import com.softdesign.devintensive.utils.DevIntensiveApplication;
+
+
+
+import com.softdesign.devintensive.data.network.req.UserLoginReq;
+import com.softdesign.devintensive.data.network.res.UserModelRes;
+
+import retrofit2.Call;
+
 /**
  * Created by alena on 28.06.16.
  */
 public class DataManager {
-    private static DataManager INSTANCE = null;
+
+    private Context mContext;
     private PreferencesManager mPreferencesManager;
+    private RestService mRestService;
+
+    private static DataManager INSTANCE = null;
+
+
+    public DataManager(){
+        this.mPreferencesManager = new PreferencesManager();
+        //this.mContext = DevIntensiveApplication.getContext();
+        this.mRestService = ServiceGenerator.createService(RestService.class);
+    }
 
     public static DataManager getInstance() {
         if (INSTANCE == null){
@@ -14,10 +41,18 @@ public class DataManager {
         return INSTANCE;
     }
 
-    private DataManager() {
-        this.mPreferencesManager = new PreferencesManager();
-    }
+
     public PreferencesManager getPreferencesManager(){
         return mPreferencesManager;
+    }
+
+    public Context getContext(){
+        return mContext;
+    }
+
+    //region --Network--
+
+    public Call<UserModelRes> loginUser (UserLoginReq userLoginReq){
+        return mRestService.loginUser(userLoginReq);
     }
 }
---
+++ app/src/main/java/com/softdesign/devintensive/data/network/RestService.java
@@ -0,0 +1,7 @@
+package com.softdesign.devintensive.data.network;
+
+/**
+ * Created by alena on 13.07.16.
+ */
+public class RestService {
+}
