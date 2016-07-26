65de538dba0cfadabc4ddce3453d25fae43f3f24
Malchish <malchih@tpu.ru>
2016-07-26 17:43:08 +0300

    Swipe to delete is done with UNDO button. New apk added.

--- app/src/main/java/com/softdesign/devintensive/ui/activities/UserListActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/UserListActivity.java
@@ -3,12 +3,22 @@ package com.softdesign.devintensive.ui.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.PersistableBundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.Loader;
 import android.support.v4.view.GravityCompat;
 ;
@@ -22,11 +32,14 @@ import android.support.v7.widget.RecyclerView;
 
 import android.support.v7.widget.Toolbar;
 
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import android.support.v7.app.ActionBar;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.SearchView;
 
 
@@ -41,6 +54,8 @@ import com.softdesign.devintensive.data.storage.models.UserDTO;
 
 import com.softdesign.devintensive.ui.adapters.UserAdapter;
 import com.softdesign.devintensive.utils.ConstantManager;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 
 import java.util.ArrayList;
@@ -50,13 +65,12 @@ import retrofit2.Response;
 
 
 
-public class UserListActivity extends AppCompatActivity  {
+public class UserListActivity extends AppCompatActivity {
 
     private ChronosConnector mConnector;
     private static final String TAG = ConstantManager.TAG_PREFIX + "UserListActivity";
     private CoordinatorLayout mCoordinatorLayout;
     private Toolbar mToolbar;
-
 
 
     private DrawerLayout mNavigationDrawer;
@@ -67,10 +81,13 @@ public class UserListActivity extends AppCompatActivity  {
     private List<User> mUserSearch;
     private static Response<UserListRes> mUserListResCall;
     private static int sPositionItemUser;
-    private MenuItem mSearchItem;
+    private MenuItem mSearchItem, mUndoBtn;
     private String mQuery;
-    //private static DataManager mAsyncLoader;
+    private AppBarLayout mAppBarLayout;
+    private ImageView mUserAvatar;
+    private NavigationView mNavigationView;
 
+    //private static DataManager mAsyncLoader;
 
 
     @Override
@@ -82,23 +99,41 @@ public class UserListActivity extends AppCompatActivity  {
         mConnector.onCreate(this, savedInstanceState);
 
         mDataManager = DataManager.getInstance();
-        mCoordinatorLayout = (CoordinatorLayout)findViewById(R.id.main_coordinator_container);
-        mToolbar = (Toolbar)findViewById(R.id.toolbar);
-        mNavigationDrawer = (DrawerLayout)findViewById(R.id.navigation_drawer);
-        mRecyclerView = (RecyclerView)findViewById(R.id.user_list);
-
-
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_userList);
+        mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mRecyclerView = (RecyclerView) findViewById(R.id.user_list);
+        mAppBarLayout = (AppBarLayout)findViewById(R.id.appBar_layout_userList);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mUserAvatar = (ImageView) mNavigationView.getHeaderView(0).findViewById(R.id.avatar_img);
         LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
-
+/*-------------------------------------------------------------------*/
+        setUpItemTouchHelper();
+        setUpAnimationDecoratorHelper();
+/*-------------------------------------------------------------------*/
         setUpToolBar();
         setUpDrawer();
 
         mConnector.runOperation(new MyChronosActivity(), TAG, true);
 
+        Picasso.with(this)
+                .load(mDataManager.getPreferencesManager().loadUserAvatar())
+                .placeholder(R.drawable.arni)
+                .into(mUserAvatar, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        ImageHelper imageHelper = new ImageHelper();
+                        Bitmap originalIcon = ((BitmapDrawable)mUserAvatar.getDrawable()).getBitmap();
+                        Bitmap roundedIcon = imageHelper.getRoundedCornerBitmap(originalIcon, originalIcon.getHeight());
+                        mUserAvatar.setImageBitmap(roundedIcon);
+                    }
 
+                    @Override
+                    public void onError() {
 
-
+                    }
+                });
     }
 
     @Override
@@ -119,18 +154,18 @@ public class UserListActivity extends AppCompatActivity  {
         super.onPause();
     }
 
-   public void onOperationFinished(final MyChronosActivity.Result result) {
-       if (result.isSuccessful()) {
-           showUsers(result.getOutput());
-       } else {
-           showSnackBar("Список пользователей не может быть загружен");
-       }
-   }
+    public void onOperationFinished(final MyChronosActivity.Result result) {
+        if (result.isSuccessful()) {
+            showUsers(result.getOutput());
+        } else {
+            showSnackBar("Список пользователей не может быть загружен");
+        }
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (item.getItemId() == android.R.id.home){
+        if (item.getItemId() == android.R.id.home) {
             mNavigationDrawer.openDrawer(GravityCompat.START);
         }
 
@@ -146,14 +181,36 @@ public class UserListActivity extends AppCompatActivity  {
         setSupportActionBar(mToolbar);
         ActionBar actionBar = getSupportActionBar();
 
-        if (actionBar == null){
+
+        if (actionBar != null) {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_dehaze_black_24dp);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-    }
-    private void setUpDrawer() {
+        actionBar.setTitle("Команда");
 
-        //TODO: реализовать переход в другую активность при клике по элементу меню в NavigationDrawer
+
+    }
+
+    private void setUpDrawer() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                switch(item.getItemId()) {
+                    case R.id.team_menu:
+                        break;
+                    case R.id.user_profile_menu:
+                        Intent a = new Intent(UserListActivity.this, MainActivity.class);
+                        startActivity(a);
+                        break;
+                }
+
+                item.setChecked(true);
+                mNavigationDrawer.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
     }
 
 
@@ -162,7 +219,10 @@ public class UserListActivity extends AppCompatActivity  {
     public boolean onPrepareOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.search_menu, menu);
+
         mSearchItem = menu.findItem(R.id.search_action);
+        //mUndoBtn = menu.findItem(R.id.menu_item_undo_checkbox);
+
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(mSearchItem);
         searchView.setQueryHint("Введите имя пользователя");
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -181,7 +241,7 @@ public class UserListActivity extends AppCompatActivity  {
         return super.onPrepareOptionsMenu(menu);
     }
 
-    private void showUsers(List<User> users){
+    private void showUsers(List<User> users) {
         mUser = users;
         mUserAdapter = new UserAdapter(mUser, new UserAdapter.CustomClickListener() {
             @Override
@@ -194,9 +254,160 @@ public class UserListActivity extends AppCompatActivity  {
         });
         mRecyclerView.swapAdapter(mUserAdapter, false);
     }
-    private void showUsersByQuery(String query){
+
+    private void showUsersByQuery(String query) {
         mQuery = query;
 
         showUsers(mDataManager.getUserListByName(query));
+    }
+
+    /* Swipe to scroll */
+
+    private void setUpItemTouchHelper() {
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // we want to cache these and not allocate anything repeatedly in the onChildDraw method
+            Drawable background;
+            boolean initiated;
+
+            private void init() {
+                background = new ColorDrawable(Color.WHITE);
+                initiated = true;
+            }
+
+            // not important, we don't want drag & drop
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public int getSwipeDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                int position = viewHolder.getAdapterPosition();
+                UserAdapter testAdapter = (UserAdapter) recyclerView.getAdapter();
+
+                if (testAdapter.isPendingRemoval(position)) {
+                    return 0;
+                }
+                return super.getSwipeDirs(recyclerView, viewHolder);
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                int swipedPosition = viewHolder.getAdapterPosition();
+                UserAdapter adapter = (UserAdapter) mRecyclerView.getAdapter();
+
+                    adapter.pendingRemoval(swipedPosition);
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
+
+                // not sure why, but this method get's called for viewholder that are already swiped away
+                if (viewHolder.getAdapterPosition() == -1) {
+                    // not interested in those
+                    return;
+                }
+
+                if (!initiated) {
+                    init();
+                }
+
+                // draw red background
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+
+        };
+        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    /**
+     * We're gonna setup another ItemDecorator that will draw the red background in the empty space while the items are animating to thier new positions
+     * after an item is removed.
+     */
+    private void setUpAnimationDecoratorHelper() {
+        mRecyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+
+            // we want to cache this and not allocate anything repeatedly in the onDraw method
+            Drawable background;
+            boolean initiated;
+
+            private void init() {
+                background = new ColorDrawable(Color.WHITE);
+                initiated = true;
+            }
+
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+
+                if (!initiated) {
+                    init();
+                }
+
+                // only if animation is in progress
+                if (parent.getItemAnimator().isRunning()) {
+
+                    // some items might be animating down and some items might be animating up to close the gap left by the removed item
+                    // this is not exclusive, both movement can be happening at the same time
+                    // to reproduce this leave just enough items so the first one and the last one would be just a little off screen
+                    // then remove one from the middle
+
+                    // find first child with translationY > 0
+                    // and last one with translationY < 0
+                    // we're after a rect that is not covered in recycler-view views at this point in time
+                    View lastViewComingDown = null;
+                    View firstViewComingUp = null;
+
+                    // this is fixed
+                    int left = 0;
+                    int right = parent.getWidth();
+
+                    // this we need to find out
+                    int top = 0;
+                    int bottom = 0;
+
+                    // find relevant translating views
+                    int childCount = parent.getLayoutManager().getChildCount();
+                    for (int i = 0; i < childCount; i++) {
+                        View child = parent.getLayoutManager().getChildAt(i);
+                        if (child.getTranslationY() < 0) {
+                            // view is coming down
+                            lastViewComingDown = child;
+                        } else if (child.getTranslationY() > 0) {
+                            // view is coming up
+                            if (firstViewComingUp == null) {
+                                firstViewComingUp = child;
+                            }
+                        }
+                    }
+
+                    if (lastViewComingDown != null && firstViewComingUp != null) {
+                        // views are coming down AND going up to fill the void
+                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    } else if (lastViewComingDown != null) {
+                        // views are going down to fill the void
+                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
+                        bottom = lastViewComingDown.getBottom();
+                    } else if (firstViewComingUp != null) {
+                        // views are coming up to fill the void
+                        top = firstViewComingUp.getTop();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    }
+
+                    background.setBounds(left, top, right, bottom);
+                    background.draw(c);
+
+                }
+                super.onDraw(c, parent, state);
+            }
+
+        });
     }
 }
--- app/src/main/java/com/softdesign/devintensive/ui/adapters/UserAdapter.java
+++ app/src/main/java/com/softdesign/devintensive/ui/adapters/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.softdesign.devintensive.ui.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -12,28 +13,39 @@ import android.widget.TextView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.data.managers.DataManager;
-import com.softdesign.devintensive.data.network.res.UserListRes;
 import com.softdesign.devintensive.data.storage.models.User;
 import com.softdesign.devintensive.ui.view.AspectRatioImageView;
 import com.softdesign.devintensive.utils.ConstantManager;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.NetworkPolicy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import android.os.Handler;
 
 /**
  * Created by alena on 15.07.16.
  */
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+    /* fields for swipe to remove feature*/
+    private static final int PENDING_REMOVAL_TIMEOUT = 3000;
+    List<User> itemsPendingRemoval;
+
+    private Handler handler = new Handler(); // hanlder for running delayed runnables
+    HashMap<User, Runnable> pendingRunnables = new HashMap<>(); // map of items to pending runnables, so we can cancel a removal if need be
+/*--------------------------------------------------------------------------------*/
     private static final String TAG = ConstantManager.TAG_PREFIX + "UserAdapter";
     private Context mContext;
     private List<User> mUsers;
     private CustomClickListener mCustomClickListener;
-
+    private View infoPanel;
 
     public UserAdapter(List<User> users, CustomClickListener customClickListener) {
         mUsers = users;
         this.mCustomClickListener = customClickListener;
+        itemsPendingRemoval = new ArrayList<>();
     }
 
 
@@ -43,14 +55,15 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
 
         mContext = parent.getContext();
         View convertView  = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user_list, parent, false);
-
         return new UserViewHolder(convertView, mCustomClickListener);
 
     }
 
     @Override
     public void onBindViewHolder(final UserAdapter.UserViewHolder holder, int position) {
-
+/*------------------------------------------------------ */
+        UserViewHolder viewHolder = holder;
+/*------------------------------------------------------ */
         final User user = mUsers.get(position);
         final String userPhoto;
 
@@ -110,7 +123,42 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         }
 
 
+    /* swipe to delete*/
+        if (itemsPendingRemoval.contains(user)) {
+            // we need to show the "undo" state of the row
 
+            viewHolder.itemView.setBackgroundColor(Color.WHITE);
+            viewHolder.mFullName.setVisibility(View.GONE);
+            viewHolder.userPhoto.setVisibility(View.GONE);
+            viewHolder.mBio.setVisibility(View.GONE);
+            viewHolder.infoPanel.setVisibility(View.GONE);
+            viewHolder.lineView.setVisibility(View.GONE);
+            viewHolder.mShowMore.setVisibility(View.GONE);
+            viewHolder.undoButton.setVisibility(View.VISIBLE);
+            viewHolder.undoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // user wants to undo the removal, let's cancel the pending task
+                    Runnable pendingRemovalRunnable = pendingRunnables.get(user);
+                    pendingRunnables.remove(user);
+                    if (pendingRemovalRunnable != null) handler.removeCallbacks(pendingRemovalRunnable);
+                    itemsPendingRemoval.remove(user);
+                    // this will rebind the row in "normal" state
+                    notifyItemChanged(mUsers.indexOf(user));
+                }
+            });
+        } else {
+            // we need to show the "normal" state
+            viewHolder.itemView.setBackgroundColor(Color.WHITE);
+            viewHolder.mFullName.setVisibility(View.VISIBLE);
+            viewHolder.userPhoto.setVisibility(View.VISIBLE);
+            viewHolder.mBio.setVisibility(View.VISIBLE);
+            viewHolder.infoPanel.setVisibility(View.VISIBLE);
+            viewHolder.lineView.setVisibility(View.VISIBLE);
+            viewHolder.mShowMore.setVisibility(View.VISIBLE);
+            viewHolder.undoButton.setVisibility(View.GONE);
+            viewHolder.undoButton.setOnClickListener(null);
+        }
 
     }
 
@@ -119,11 +167,15 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         return mUsers.size();
     }
     public static class UserViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+/* -------------------------------------------*/
+        Button undoButton;
+/* -------------------------------------------*/
         protected AspectRatioImageView userPhoto;
         protected TextView mFullName, mRating, mCodeLines, mProjects, mBio;
         protected Button mShowMore;
-
+        protected View infoPanel;
         protected Drawable mDummy;
+        protected View lineView;
 
         private CustomClickListener mCustomClickListener;
 
@@ -131,6 +183,8 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
             super(itemView);
             this.mCustomClickListener = customClickListener;
 
+            lineView = (View)itemView.findViewById(R.id.line);
+            infoPanel = (View) itemView.findViewById(R.id.info_panel);
             userPhoto = (AspectRatioImageView) itemView.findViewById(R.id.user_photo_img);
             mFullName = (TextView) itemView.findViewById(R.id.user_full_name_txt);
             mRating = (TextView) itemView.findViewById(R.id.rating_txt);
@@ -138,6 +192,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
             mProjects = (TextView) itemView.findViewById(R.id.projects_txt);
             mBio = (TextView) itemView.findViewById(R.id.bio_txt);
             mShowMore = (Button)itemView.findViewById(R.id.more_info_btn);
+            undoButton = (Button) itemView.findViewById(R.id.undo_button);
 
             mDummy = userPhoto.getContext().getResources().getDrawable(R.drawable.login_bg);
             mShowMore.setOnClickListener(this);
@@ -153,5 +208,39 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
 
     public interface CustomClickListener{
         void onUserItemClickListener(int position);
+    }
+
+    public void pendingRemoval(int position) {
+        final User user = mUsers.get(position);
+        if (!itemsPendingRemoval.contains(user)) {
+            itemsPendingRemoval.add(user);
+            // this will redraw row in "undo" state
+            notifyItemChanged(position);
+            // let's create, store and post a runnable to remove the item
+            Runnable pendingRemovalRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    remove(mUsers.indexOf(user));
+                }
+            };
+            handler.postDelayed(pendingRemovalRunnable, PENDING_REMOVAL_TIMEOUT);
+            pendingRunnables.put(user, pendingRemovalRunnable);
+        }
+    }
+
+    public void remove(int position) {
+        final User user = mUsers.get(position);
+        if (itemsPendingRemoval.contains(user)) {
+            itemsPendingRemoval.remove(user);
+        }
+        if (mUsers.contains(user)) {
+            mUsers.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    public boolean isPendingRemoval(int position) {
+        User user = mUsers.get(position);
+        return itemsPendingRemoval.contains(user);
     }
 }
